76b7ee3829bb09c162348860f43cc37c3f89bae0
Furkan <furkan.kaplan@gmail.com>
2021-11-02 17:40:31 +0300

    Implement all state

--- src/App.js
+++ src/App.js
@@ -1,29 +1,156 @@
-import {Container, Grid, Icon, Segment} from 'semantic-ui-react'
-import './App.css';
-import MainHeader from './components/MainHeader';
-import NewEntryForm from './components/NewEntryForm';
-import DisplayBalance from './components/DisplayBalance';
-import DisplayBalances from './components/DisplayBalances';
-import EntryLine from './components/EntryLine';
+import { useState, useEffect } from "react";
+import { Container } from "semantic-ui-react";
+import "./App.css";
+import MainHeader from "./components/MainHeader";
+import NewEntryForm from "./components/NewEntryForm";
+import DisplayBalance from "./components/DisplayBalance";
+import DisplayBalances from "./components/DisplayBalances";
+import EntryLines from "./components/EntryLines";
+import ModalEdit from "./components/ModalEdit";
 
 function App() {
+  const [entries, setEntries] = useState(initialEntries);
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
+  const [isExpense, setIsExpense] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [entryId, setEntryId] = useState();
+  const [incomeTotal, setIncomeTotal] = useState(0);
+  const [expenseTotal, setExpenseTotal] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    if(!isOpen && entryId){
+      const index = entries.findIndex(entry => entry.id === entryId);
+      const newEntries = [...entries];
+      newEntries[index].description = description;
+      newEntries[index].value = value;
+      newEntries[index].isExpense = isExpense;
+      setEntries(newEntries);
+      setDescription('');
+      setValue('');
+      setIsExpense(true);
+    }
+  }, [isOpen])
+
+  useEffect(() => {
+    let totalIncome = 0;
+    let totalExpense = 0;
+    entries.map(entry => {
+      if(entry.isExpense){
+        return totalExpense += Number(entry.value);
+      }else{
+        return totalIncome += Number(entry.value);
+      }
+    });
+    setTotal(totalIncome - totalExpense);
+    setExpenseTotal(totalExpense);
+    setIncomeTotal(totalIncome);
+  }, [entries])
+
+  //const deleteEntry = (id) => {}
+  function deleteEntry(id) {
+    const result = entries.filter((entry) => entry.id !== id);
+    setEntries(result);
+  }
+
+  function editEntry(id) {
+    console.log(`edit entry with id ${id}`);
+    if (id) {
+      const index = entries.findIndex(entry => entry.id === id);
+      const entry = entries[index];
+      setEntryId(id);
+      setDescription(entry.description);
+      setValue(entry.value);
+      setIsExpense(entry.isExpense);
+      setIsOpen(true);
+    }
+  }
+
+  function addEntry() {
+    const result = entries.concat({
+      id: entries.length + 1,
+      description,
+      value,
+      isExpense,
+    });
+    setEntries(result);
+    resetEntry();
+  }
+
+  function resetEntry(){
+    setDescription("");
+    setValue("");
+    setIsExpense("");
+  }
+
   return (
     <Container>
       <MainHeader title="Budget"></MainHeader>
 
-      <DisplayBalance title="Expense" value="2,550.53" size="small"/> 
-      <DisplayBalances />
+      <DisplayBalance title="Your Balance" value={total} size="small" />
+      <DisplayBalances incomeTotal={incomeTotal} expenseTotal={expenseTotal} />
 
       <MainHeader title="History" type="h3"></MainHeader>
 
-      <EntryLine description="Income" value="$10.00"/>
-      <EntryLine description="Expense" value="$10.00" isExpense/>
+      <EntryLines
+        entries={entries}
+        deleteEntry={deleteEntry}
+        editEntry={editEntry}
+      />
 
       <MainHeader title="Add new transaction" type="h3"></MainHeader>
 
-      <NewEntryForm/>
+      <NewEntryForm
+        addEntry={addEntry}
+        description={description}
+        value={value}
+        isExpense={isExpense}
+        setDescription={setDescription}
+        setValue={setValue}
+        setIsExpense={setIsExpense}
+      />
+
+      <ModalEdit
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        addEntry={addEntry}
+        description={description}
+        value={value}
+        isExpense={isExpense}
+        setDescription={setDescription}
+        setValue={setValue}
+        setIsExpense={setIsExpense}
+      />
     </Container>
   );
 }
 
 export default App;
+
+var initialEntries = [
+  {
+    id: 1,
+    description: "Work income",
+    value: 1000.00,
+    isExpense: false,
+  },
+  {
+    id: 2,
+    description: "Water bill",
+    value: 20,
+    isExpense: true,
+  },
+  {
+    id: 3,
+    description: "Rent",
+    value: 300.00,
+    isExpense: true,
+  },
+  {
+    id: 4,
+    description: "Power bill",
+    value: 50,
+    isExpense: true,
+  },
+];
--- src/components/NewEntryForm.js
+++ src/components/NewEntryForm.js
@@ -1,27 +1,33 @@
-import React from 'react'
-import {Form} from 'semantic-ui-react'
-import ButtonSaveOrCancel from './ButtonSaveOrCancel';
+import React from "react";
+import { Form } from "semantic-ui-react";
+import ButtonSaveOrCancel from "./ButtonSaveOrCancel";
+import EntryForm from "./EntryForm";
 
-function NewEntryForm() {
-    return (
-        <Form unstackable>
-            <Form.Group>
-            <Form.Input 
-                icon="tags" 
-                width={12} 
-                label="Description" 
-                placeholder="New shinny thing"/>
-            <Form.Input 
-                width={4}
-                label="Value"
-                placeholder="100.00"
-                icon="dollar"
-                iconPosition="left"
-                />
-            </Form.Group>
-            <ButtonSaveOrCancel/>
-      </Form>
-    )
+function NewEntryForm({
+  addEntry,
+  description,
+  value,
+  isExpense,
+  setDescription,
+  setValue,
+  setIsExpense,
+}) {
+  return (
+    <Form unstackable>
+      <EntryForm
+        description={description}
+        value={value}
+        isExpense={isExpense}
+        setDescription={setDescription}
+        setValue={setValue}
+        setIsExpense={setIsExpense}
+      />
+
+      <ButtonSaveOrCancel
+        addEntry={addEntry}
+      />
+    </Form>
+  );
 }
 
-export default NewEntryForm
+export default NewEntryForm;
